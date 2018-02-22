c9f2b7901ad5d1d788ef6dc746e9e45897ad84f0
Ryan T. Hamilton <astrobokonon@gmail.com>
2018-02-22 15:44:54 -0700

    License blurb added

--- servants/utils/alarms.py
+++ servants/utils/alarms.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Feb 22 14:40:01 2018
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-@author: rhamilton
+  Created on Thu Feb 22 14:40:01 2018
+
+  @author: rhamilton
 """
 
 from __future__ import division, print_function, absolute_import
