87a79b2fd9c4cae47a3d47ebf324d80609518086
Ryan Hamilton <rhamilton@lowell.edu>
2018-02-23 14:02:48 -0700

    Adding in boilerplate at top of database lib code

--- servants/utils/database.py
+++ servants/utils/database.py
@@ -1,3 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+  Created on Thu Feb 22 20:05:07 2018
+
+  @author: rhamilton
+"""
+
+from __future__ import division, print_function, absolute_import
+
 import numpy as np
 import datetime as dt
 
