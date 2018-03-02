ad52dfa695fb48a2e7ba96d6c8a0e24c94c2428e
Ryan T. Hamilton <rhamilton@lowell.edu>
2018-03-02 12:18:23 -0700

    Updated sched_model to figure out best way to queue actions

--- toymodels/sched_model.py
+++ toymodels/sched_model.py
@@ -19,22 +19,16 @@ import datetime as dt
 from pid import PidFile, PidFileError
 
 
-def func1(arg1=None, arg2='world'):
-    print("Func 1!")
-    diff = dt.datetime.utcnow() - arg1
-    print(diff.total_seconds())
+class processDescription():
+    def __init__(self, **kwargs):
+        self.func = None
+        self.timedelay = 0.
+        self.priority = 5
+        self.args = []
+        self.kwargs = {}
 
-
-def func2(arg1='hello', arg2='world'):
-    print("Func 2!")
-    diff = dt.datetime.utcnow() - arg1
-    print(diff.total_seconds())
-
-
-def func3(arg1='hello', arg2='world'):
-    print("Func 3!")
-    diff = dt.datetime.utcnow() - arg1
-    print(diff.total_seconds())
+        for each in kwargs:
+            setattr(self, each, kwargs[each])
 
 
 class TimeOutException(Exception):
@@ -42,56 +36,91 @@ class TimeOutException(Exception):
         Exception.__init__(self, *args, **kwargs)
 
 
+def func1(arg1, arg2='world'):
+    print("Func %s!" % (arg2))
+    diff = dt.datetime.utcnow() - arg1
+    print(diff.total_seconds())
+    # This sleep should be long enough that it causes a conflict in timing
+    #   between func3 and func2 to illustrate the sequencing with delays
+    time.sleep(25)
+
+
+def func2(arg1, arg2='world'):
+    print("Func %s!" % (arg2))
+    diff = dt.datetime.utcnow() - arg1
+    print(diff.total_seconds())
+    time.sleep(15)
+
+
+def func3(arg1, arg2='world'):
+    print("Func %s!" % (arg2))
+    diff = dt.datetime.utcnow() - arg1
+    print(diff.total_seconds())
+
+
 def gotAlarm(signum, frame):
     print("Took too long! Raising TimeOutException.")
     raise TimeOutException
 
 
-def scheduleMaster(s):
+def scheduleMaster(s, actlist=[processDescription()]):
     print("Making the schedule")
 
-    ival1 = 10.
-    ival2 = 15.
-    ival3 = 30.
-    st = dt.datetime.utcnow()
+    rt = dt.datetime.utcnow()
 
-    ev1 = s.enter(ival1, priority=1, action=func1,
-                  kwargs={'arg1': st, 'arg2': 'one'})
-    ev2 = s.enter(ival2, priority=1, action=func2,
-                  kwargs={'arg1': st, 'arg2': 'two'})
-    ev3 = s.enter(ival3, priority=2, action=func3,
-                  kwargs={'arg1': st, 'arg2': 'three'})
+    elist = []
+    for i, each in enumerate(actlist):
+        print("Function %d" % (i))
+        # Temporary hack to give the relative time since this function started
+        #   to help in debugging the actual timing/sequence of things
+        each.args = [rt]
+        # Note that priority only takes effect if two events are *scheduled*
+        #   for the same time; if there's a delay in one function, it
+        #   won't really matter which of the next tasks has a higher priority
+        event = s.enter(each.timedelay, priority=each.priority,
+                        action=each.func,
+                        argument=each.args, kwargs=each.kwargs)
+        print(event)
+        elist.append(event)
 
-    print(ev1)
-    print(ev2)
-    print(ev3)
     print("Scheduler start")
     s.run()
     print("Scheduler done")
 
 
 if __name__ == "__main__":
+    # Define our desired actions within the main loop
+    v1 = 1
+    v2 = dt.datetime.utcnow()
+    act1 = processDescription(func=func1, timedelay=10., priority=3,
+                              args=[v1, v2], kwargs={'arg2': 'one'})
+    act2 = processDescription(func=func2, timedelay=20., priority=2,
+                              args=[v1, v2], kwargs={'arg2': 'two'})
+    act3 = processDescription(func=func3, timedelay=30., priority=1,
+                              args=[v1, v2], kwargs={'arg2': 'thr'})
     try:
         with PidFile(pidname='test', piddir='/tmp') as p:
-            print(p.pid)
-            print(p.filename)
-            print(p.piddir)
+            # Print PIDFile info
+            print(p.pid, p.filename)
+
+            # Setup signal handler
+            signal.signal(signal.SIGALRM, gotAlarm)
+
             # Set schedule handler
             s = sched.scheduler(time.time, time.sleep)
 
-            # Set signal handler
-            signal.signal(signal.SIGALRM, gotAlarm)
-
-            # Actually arm the alarm
-            alarmtime = 30
-            signal.alarm(alarmtime)
-            print("Set an alarm for %d seconds" % (alarmtime))
-
             try:
-                scheduleMaster(s)
+                # Actually arm the alarm just before we start
+                alarmtime = 30
+                signal.alarm(alarmtime)
+                print("Set an alarm for %d seconds" % (alarmtime))
+                # Act out all the actions, each within their
+                scheduleMaster(s, [act1, act2, act3])
             except TimeOutException:
                 print("Giving up.")
-            # Disable the alarm
+
+            # Disable the alarm since everything went ok
             signal.alarm(0)
     except PidFileError as err:
+        print("Process already running! Close the other one.")
         print(str(err))
