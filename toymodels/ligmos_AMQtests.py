28ab3ad5b9a391b47cc17872b5870fcc6717df38
Ryan T. Hamilton <astrobokonon@gmail.com>
2018-09-13 12:45:53 -0700

    Quality of life updates for AMQ stats testing

--- toymodels/ligmos_AMQtests.py
+++ toymodels/ligmos_AMQtests.py
@@ -30,7 +30,8 @@ from ligmos import workers
 if __name__ == "__main__":
     log = logging.getLogger('stomp.py')
     bigsleep = 10
-    brokerhost = "10.10.30.137"
+    # brokerhost = "10.11.131.241"
+    brokerhost = 'joe.lowell.edu'
     brokerport = 61613
 
     # ActiveMQ connection checker
@@ -38,8 +39,11 @@ if __name__ == "__main__":
 
     crackers = utils.amq.ParrotSubscriber()
 
+    statsRequest = 'ActiveMQ.Statistics.Broker'
+    statsAnswer = '/queue/stats.broker'
+
     # Collect the activemq topics that are desired
-    alltopics = ['lmiInternalTelemetry']
+    alltopics = [statsRequest, statsAnswer]
 
     # Establish connections and subscriptions w/our helper
     # TODO: Figure out how to fold in broker passwords
@@ -52,27 +56,31 @@ if __name__ == "__main__":
                                connect=False)
 
     # Semi-infinite loop
-    while True:
-        # Double check that the broker connection is still up
-        #   NOTE: conn.connect() handles ConnectionError exceptions
-        if conn.conn is None:
-            print("No connection at all! Retrying...")
-            conn.connect()
-        elif conn.conn.transport.connected is False:
-            # Added the "first" flag to take care of a weird bug
-            print("Connection died! Reestablishing...")
-            conn.connect()
-        else:
-            print("Connection still valid")
+    try:
+        while True:
+            # Double check that the broker connection is still up
+            #   NOTE: conn.connect() handles ConnectionError exceptions
+            if conn.conn is None:
+                print("No connection at all! Retrying...")
+                conn.connect()
+            elif conn.conn.transport.connected is False:
+                # Added the "first" flag to take care of a weird bug
+                print("Connection died! Reestablishing...")
+                conn.connect()
+            else:
+                print("Connection still valid")
 
-        # If we're here, we made it once thru. The above comparison
-        #   will fail without this and we'll never reconnect!
-        first = False
+            # If we're here, we made it once thru. The above comparison
+            #   will fail without this and we'll never reconnect!
+            first = False
+            conn.publish(statsRequest, "", replyto=statsAnswer, debug=False)
 
-        print("Starting a big sleep")
-        # Sleep for bigsleep, but in small chunks to check abort
-        for i in range(bigsleep):
-            time.sleep(1)
-
-    # Disconnect from the ActiveMQ broker
-    conn.disconnect()
+            print("Starting a big sleep")
+            # Sleep for bigsleep, but in small chunks to check abort
+            for i in range(bigsleep):
+                time.sleep(1)
+    except:
+        # A bit sloppy to catch control-C in the laziest way
+        # Disconnect from the ActiveMQ broker
+        conn.disconnect()
+        print("Really disconnected from %s" % (brokerhost))
