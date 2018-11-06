74e2129b41d430c2cbd00c1ff79493418d9280fc
Ryan T. Hamilton <astrobokonon@gmail.com>
2018-11-06 13:21:36 -0700

    Check external ping enabled status

--- Alfred.py
+++ Alfred.py
@@ -196,17 +196,22 @@ def main():
                                             alarmtime=alarmtime)
 
                 # Doing the extra pings as a side job/quickie
-                #   No need to spin up a whole big thing, this is quicker
+                #   No need to make this into a big to-do
                 if epings is not None:
                     for sect in epings.sections():
-                        pobj = utils.common.baseTarget()
-                        # Bit of a quick-and-dirty parse here, will fail
-                        #   on typos/capitalization problems...
-                        # TODO: Check enabled status rather than ignoring it.
-                        pobj.host = epings[sect]['host']
-                        pobj.port = epings[sect]['port']
+                        # Check to see if this section is "enabled"
+                        penab = epings[sect].getboolean('enabled')
+                        if penab is True:
+                            pobj = utils.common.baseTarget()
+                            # Bit of a quick-and-dirty parse here, will fail
+                            #   on typos/capitalization problems...
+                            pobj.host = epings[sect]['host']
+                            pobj.port = epings[sect]['port']
 
-                        alfred.tasks.actionPing(pobj, db=idb, debug=args.debug)
+                            alfred.tasks.actionPing(pobj, db=idb,
+                                                    debug=args.debug)
+                        else:
+                            print("Ping %s is disabled in conf file!" % (sect))
 
                 # After all the instruments are done, take a big nap
                 if runner.halt is False:
