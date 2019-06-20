79784dfe80391bf8df8a7e7d463a56b77ef1d73f
Ryan T. Hamilton <astrobokonon@gmail.com>
2019-06-20 15:44:20 -0700

    Update confDebugging for new ligmos setup

--- confDebugging.py
+++ confDebugging.py
@@ -1,21 +1,14 @@
-from ligmos.utils import confparsers, confutils, classes
-
+from ligmos.utils import confparsers, classes
 
 cfile = './alfred.conf'
-ctype = classes.hostTarget
+confclass = classes.hostTarget
 pfile = './passwords.conf'
 
 # Call the parser DIRECTLY which returns a dict of configparser info in 'conf'
-idict, comm = confparsers.parseConfFile(cfile,
-                                        commonBlocks=True,
-                                        enableCheck=True,
-                                        debug=True)
+config, comm = confparsers.parseConfig(cfile, confclass,
+                                       passfile=pfile,
+                                       searchCommon=True,
+                                       enableCheck=True,
+                                       debug=True)
 
-# Call the helper, which fills a class of type ctype along the way. 'conf2'
-#   is then a dict with keys of section names and vals of filled instances
-#   of type ctype
-idict2, comm2 = confutils.parseConfPasses(cfile, pfile,
-                                          conftype=ctype)
-
-
-print(idict2, comm2)
+print(config, comm)
