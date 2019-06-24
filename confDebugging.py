c16844cf67e849f62a47b3203b8f14f3f1d7a58f
Ryan T. Hamilton <astrobokonon@gmail.com>
2019-06-24 13:34:54 -0700

    Moved on to that great repo in the cloud

--- confDebugging.py
+++ confDebugging.py
@@ -1,52 +1,5 @@
 from ligmos.utils import amq, classes, database, confparsers
-
-
-def establishAMQIDBConnections(comm):
-    """
-    Set up the actual connections, which we'll then give back to the actual
-    objects for them to do stuff with afterwards.
-    """
-    amqbrokers = {}
-    influxdatabases = {}
-
-    for commsection in comm:
-        # Rename for easier access/passing
-        cobj = comm[commsection]
-
-        # Now check the properties of this object to see if it's something we
-        #   actually regconize and then connect to
-        if cobj.type.lower() == 'activemq':
-            # We get brokerlistener back as a return just in case it was
-            #   None initially, in which case amq.setupBroker would give one
-            conn, amqlistener = amq.setupAMQBroker(cobj,
-                                                   amqtopics[commsection],
-                                                   listener=amqlistener)
-
-            # Store this so we can check/use it later
-            brokerbits = [conn, amqlistener]
-            amqbrokers.update({commsection: brokerbits})
-        elif cobj.type.lower() == 'influxdb':
-            # Create an influxdb object that can be spread around to
-            #   connect and commit packets when they're created.
-            #   Leave it disconnected initially.
-            idb = database.influxobj(database=None,
-                                     host=cobj.host,
-                                     port=cobj.port,
-                                     user=cobj.user,
-                                     pw=cobj.password,
-                                     connect=False)
-
-            # Connect briefly to check/verify everything is working
-            idb.connect()
-            idb.disconnect()
-
-            # Store this so we can check/use it later
-            influxdatabases.update({commsection: idb})
-        else:
-            # No other types are defined yet
-            pass
-
-    return amqbrokers, influxdatabases
+from ligmos.workers import connSetup
 
 
 cfile = './alfred.conf'
@@ -73,7 +26,8 @@ amqlistener = amq.silentSubscriber()
 #   above, but even that can be hidden away if we really need/want to do that.
 
 amqtopics = amq.getAllTopics(config, comm)
-amqbrokers, influxdatabases = establishAMQIDBConnections(comm)
+amqbrokers, influxdatabases = connSetup.establishAMQIDBConnections(comm,
+                                                                   amqtopics)
 
 # This part should be in whatever loop gets set up to continually run
 #   amqbroker sections containing the connection objects can change, so
