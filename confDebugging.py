892664cdc724ddec486e7ffd7c4a0f1d03d2406b
Ryan T. Hamilton <astrobokonon@gmail.com>
2019-06-24 13:11:47 -0700

    Move more functional stuff over to ligmos

--- confDebugging.py
+++ confDebugging.py
@@ -1,40 +1,10 @@
 from ligmos.utils import amq, classes, database, confparsers
 
 
-def checkAMQConnections(amqbrokers, subscribe=True):
-    """
-    This is intended to be inside of some loop structure.
-    It's primarily used for checking whether the connection to the ActiveMQ
-    broker is still valid, and, if it was killed (set to None) because the
-    heartbeat failed, attempt to both reconnect and resubscribe to the
-    topics.
-    """
-    for bconn in amqbrokers:
-        # From above, amqbrokers is a dict with values that are a list of
-        #   connection object, list of topics, listener object
-        connChecking = amqbrokers[bconn][0]
-        thisListener = amqbrokers[bconn][1]
-        if connChecking.conn is None:
-            print("No connection at all! Retrying...")
-            # The topics were already stuffed into the connChecking object,
-            #   but it's nice to remember that we're subscribing to them
-            connChecking.connect(listener=thisListener, subscribe=subscribe)
-        elif connChecking.conn.transport.connected is False:
-            print("Connection died! Reestablishing...")
-            connChecking.connect(listener=thisListener, subscribe=subscribe)
-        else:
-            print("Connection still valid")
-
-        # Make sure we save any connection changes and give it back
-        amqbrokers[bconn] = [connChecking, thisListener]
-
-    return amqbrokers
-
-
 def establishAMQIDBConnections(comm):
     """
     Set up the actual connections, which we'll then give back to the actual
-    objects for them to do stuff with afterwards
+    objects for them to do stuff with afterwards.
     """
     amqbrokers = {}
     influxdatabases = {}
@@ -79,42 +49,6 @@ def establishAMQIDBConnections(comm):
     return amqbrokers, influxdatabases
 
 
-def getAllAMQTopics(config):
-    """
-    Since we can have multiple connections to a single broker or database or
-    whatever, we need to search through all of our defined sections to
-    pull out some of the specifics into one place; that makes our
-    connection/reconnection/subscription logic way easier
-    """
-    amqtopics = {}
-    for sect in config:
-        csObj = config[sect]
-        try:
-            brokerTag = csObj.broker
-            brokertype = comm[brokerTag].type
-        except AttributeError:
-            # If we end up in here, we're completely hoopajooped so give up
-            break
-
-        if brokertype.lower() == 'activemq':
-            # Gather up broker stuff
-            try:
-                # First see if we have anything previously gathered, to make
-                #   sure we don't accidentally clobber anything
-                alltopics = amqtopics[brokerTag]
-            except KeyError:
-                alltopics = []
-
-            # Get the topics; it's guaranteed to be a list
-            thesetopics = amq.gatherAMQTopics(csObj)
-            alltopics += thesetopics
-
-            # list(set()) to quickly take care of any dupes
-            amqtopics.update({brokerTag: list(set(alltopics))})
-
-    return amqtopics
-
-
 cfile = './alfred.conf'
 confclass = classes.hostTarget
 pfile = './passwords.conf'
@@ -138,10 +72,10 @@ amqlistener = amq.silentSubscriber()
 #   for much easier access.  The only thing that might poke out is the stuff
 #   above, but even that can be hidden away if we really need/want to do that.
 
-amqtopics = getAllAMQTopics(config)
+amqtopics = amq.getAllTopics(config, comm)
 amqbrokers, influxdatabases = establishAMQIDBConnections(comm)
 
 # This part should be in whatever loop gets set up to continually run
 #   amqbroker sections containing the connection objects can change, so
 #   make sure that we get it back in case a change (reconnection) happened.
-amqbrokers = checkAMQConnections(amqbrokers, subscribe=False)
+amqbrokers = amq.checkConnections(amqbrokers, subscribe=False)
