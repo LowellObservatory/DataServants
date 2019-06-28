585501e55c6d7225f155b2b80ac6bfa1c6e0c222
Ryan T. Hamilton <astrobokonon@gmail.com>
2019-06-28 16:11:36 -0700

    Tear out temperature parser from Iago
    I'm giving it to Mr. Freeze; prob an argument for a ligmos parser module

--- dataservants/iago/parsetopics.py
+++ dataservants/iago/parsetopics.py
@@ -156,55 +156,7 @@ def parserLOlogs(hed, msg, db=None, badFWHM=100.):
     meas = ['InstrumentTelemetry']
     tags = {'name': topic.split(".")[1]}
 
-    if loglevel in ("Level_5", "Level_4"):
-        fields = None
-        if logmsg.startswith("CCD sensor adus"):
-            # print("Parsing: %s" % (logmsg))
-            # CCD sensor adus temp1 2248 temp2 3329 set1 2249 heat1 2016'
-            adutemp1 = int(logmsg.split(" ")[4])
-            adutemp2 = int(logmsg.split(" ")[6])
-            aduset1 = int(logmsg.split(" ")[8])
-            aduheat1 = int(logmsg.split(" ")[10])
-
-            fields = {"aduT1": adutemp1}
-            fields.update({"aduT2": adutemp2})
-            fields.update({"aduT2": adutemp2})
-            fields.update({"aduS1": aduset1})
-            fields.update({"aduH1": aduheat1})
-
-            # print(adutemp1, adutemp2, aduset1, aduheat1)
-        elif logmsg.startswith("CCD Heater"):
-            # NOTE! This one will have had a ":" removed by the
-            #   logmsg creation line above, so you can just split normally
-            # print("Parsing: %s" % (logmsg))
-            # CCD Heater Values:1.21 0.00
-            heat1 = float(logmsg.split(" ")[3])
-            heat2 = float(logmsg.split(" ")[4])
-
-            fields = {"H1": heat1}
-            fields.update({"H2": heat2})
-
-            # print(heat1, heat2)
-        elif logmsg.startswith("CCD Temp"):
-            # Same as "CCD Heater" in that ":" have been removed by this point
-            # print("Parsing: %s" % (logmsg))
-            # CCD Temp -110.06 18.54 Setpoints -109.95 0.00 '
-            temp1 = float(logmsg.split(" ")[2])
-            temp2 = float(logmsg.split(" ")[3])
-            set1 = float(logmsg.split(" ")[5])
-            set2 = float(logmsg.split(" ")[6])
-
-            fields = {"T1": temp1}
-            fields.update({"T2": temp2})
-            fields.update({"S1": set1})
-            fields.update({"S2": set2})
-            fields.update({"T1S1delta": temp1-set1})
-
-            # print(temp1, temp2, set1, set2)
-        else:
-            fields = None
-            # print(loglevel, logmsg)
-    elif loglevel == "Level_2":
+    if loglevel == "Level_2":
         fields = None
         if logmsg.startswith("SendAnalysis"):
             apts = logmsg.split()
