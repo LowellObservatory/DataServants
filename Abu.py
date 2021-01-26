62542f64e7352cf0741cb8a7ef9a64df7eb24089
Ryan T. Hamilton <astrobokonon@gmail.com>
2021-01-26 15:53:51 -0700

    Allow abu to work at the mesa too
    Hacky but whatever for now

--- Abu.py
+++ Abu.py
@@ -87,7 +87,7 @@ def main():
 
                 if wxml != '':
                     bxml = None
-                    if sect == 'ldtweather':
+                    if sect == 'ldtweather' or 'mesaweather':
                         bxml = abu.actions.parseColumbia(wxml)
                     elif sect == 'ldtigrid':
                         bxml = abu.actions.parseiSense(wxml,
