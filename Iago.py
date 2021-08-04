5d1129282947a7dacbae325a24210589c0805e49
Ryan T. Hamilton <astrobokonon@gmail.com>
2021-08-04 13:39:59 -0700

    Speed up Iago again
    Should move Mr. Freeze style scheduling over here

--- Iago.py
+++ Iago.py
@@ -34,7 +34,7 @@ def main():
     conftype = classes.snoopTarget
 
     # Interval between successive runs of the polling loop (seconds)
-    bigsleep = 120
+    bigsleep = 60
 
     # config: dictionary of parsed config file
     # comm: common block from config file
